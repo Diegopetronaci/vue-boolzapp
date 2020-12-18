82b64f6b251bb724adce9beddfbc1eafa30fc97e
Diego <Petronacidiego@gmail.com>
2020-12-18 12:56:12 +0100

    commit iniziale

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+//Milestone 1
+//Replica della grafica ​con la possibilità di avere messaggi scritti
+//dall’utente (verdi) edall’interlocutore (bianco) assegnando
+//due classi CSS diverse
+//Visualizzazione dinamica della lista contatti:​
+//tramite la direttiva v-for, visualizzarenome e immagine di ogni contatto
